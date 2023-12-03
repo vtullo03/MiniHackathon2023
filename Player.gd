fb4c75defd6ea351878f8ecbe915b274e8eb9cb6
ilayd-a <66847423+ilayd-a@users.noreply.github.com>
2023-12-03 12:55:32 -0500

    .

--- Player.gd
+++ Player.gd
@@ -6,8 +6,8 @@ extends CharacterBody2D
 var rotation_direction = 0
 
 func get_input():
-	rotation_direction = Input.get_axis("left", "right")
-	velocity = transform.x * Input.get_axis("down", "up") * speed
+	rotation_direction = Input.get_axis("move_left", "move_right")
+	velocity = transform.x * Input.get_axis("move_down", "move_up") * speed
 
 func _physics_process(delta):
 	get_input()
