20e392ec4cabc3a9a8938d80427fc653e2a4492f
vtullo03 <vitotullo430@gmail.com>
2023-12-03 12:34:25 -0500

    Bullet Object Created

---
+++ Bullet.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+var bullet_speed = 325.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += transform.x * bullet_speed * delta
